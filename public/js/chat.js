06b1c943adf3340d742de559534f24a31dcb4ad9
Alish <alishmadhukar75@gmail.com>
2020-10-21 11:12:59 -0700

    Added location feature

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,35 @@
-io()
+
+
+const socket = io()
+
+socket.on('message', (message) =>{
+    console.log(message)
+})
+
+// socket.on('countUpdated', (count) =>{
+//     console.log('The count has been updated', count)
+// })
+
+// document.querySelector('#increment').addEventListener('click', () =>{
+//     console.log('clicked')
+//     socket.emit('increment')
+// })
+
+document.querySelector('#messageForm').addEventListener('submit', (e) =>{
+    e.preventDefault()
+
+    const msg = e.target.elements.message.value
+    socket.emit('sendMessage', msg)
+})
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if(!navigator.geolocation){
+        return alert('geoloaction is not supported for your browser')
+    }
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    })
+})
--- src/index.js
+++ src/index.js
@@ -12,8 +12,30 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', () =>{
+let count = 0
+
+io.on('connection', (socket) =>{
     console.log('New websocket connections')
+
+    socket.emit('message', 'Welcome!')
+
+    socket.broadcast.emit('message', 'new user has joined the chat')
+
+    socket.on('sendMessage', (message) =>{     
+        io.emit('message', message)
+    })
+    socket.on('disconnect', () => {
+        io.emit('message', 'User left the chat')
+    })
+    socket.on('sendLocation', (coords) =>{
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+    })
+    // socket.emit('countUpdated', count)
+    // socket.on('increment', () =>{
+    //     count ++
+    //     // socket.emit('countUpdated', count)
+    //     io.emit('countUpdated', count)
+    // } )
 })
 
 Server.listen(port, () =>{
